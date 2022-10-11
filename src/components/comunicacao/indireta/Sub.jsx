a78db73b0dbd61f02bd67a0793686820d36ca2ae
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2022-10-11 16:36:21 -0300

    feat: acao function

--- src/components/comunicacao/indireta/Sub.jsx
+++ src/components/comunicacao/indireta/Sub.jsx
@@ -3,7 +3,14 @@ import { button, button_primary } from "../../../assets/button.module.css"
 
 export const Sub = (props) => {
 
+    const acao = () => {
+        props.onClicar(
+            Math.floor(Math.random() * 100),
+            'Passado de Filho para Pai'
+        )
+    }
+
     return (
-        <button className={`${button} ${button_primary}`} onClick={() => { props.onClicar(Math.random()) }}>Alterar</button>
+        <button className={`${button} ${button_primary}`} onClick={acao}> Alterar</button >
     )
 }
