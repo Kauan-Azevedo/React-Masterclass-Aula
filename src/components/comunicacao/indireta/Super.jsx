134a06ae48f44c97986d7e97caaf578699156cf0
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2022-10-11 16:54:25 -0300

    feat: Super e Sub finalizados.

--- src/components/comunicacao/indireta/Super.jsx
+++ src/components/comunicacao/indireta/Super.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Sub } from "./Sub"
 
 
 export const Super = (props) => {
 
+    const [label, setLabel] = useState("Gerado");
+    const [num, setNum] = useState(0);
+
     const handleClick = (valor, msg) => {
-        console.log(msg)
-        console.log(valor)
+        setNum(valor)
+        setLabel(msg)
     }
 
     return (
-        <div>
-            <h3>Valor</h3>
+        <div>w
+            <h3>{label}: {num}</h3>
             <Sub onClicar={handleClick}></Sub>
         </div>
     )
